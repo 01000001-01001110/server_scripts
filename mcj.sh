f0c5f2c8492a517255e5c73f244206ed560289e1
01000001-01001110 <48245017+01000001-01001110@users.noreply.github.com>
2021-03-30 14:09:03 -0400

    Create mcj.sh

---
+++ mcj.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Alan's Java MC setup script.
+#Version 0.0.1
+#Date 3/30/2021
+
+read -p "Server Name : " servername
+read -p "Game Mode, Select either creative|survival : " gamemode
+read -p "Seed : " seed
+read -p "Port : " port
+read -p "Require Microsoft Account, Select either true|false : " online
+read -p "Container Name : " contname
+read -p "Spawn NPCs, Select either true|false : " npc
+read -p "Server Monsters, Select either true|false : " monster
+read -p "Spawn animals, Select either true|false : " animal
+docker run -d -it -e EULA=TRUE --name $contname -e SERVER_NAME="$servername" -e GAMEMODE=$gamemode -e ONLINE_MODE=$online -e level_seed=$seed -e difficulty=normal -e spawn-npcs=$npc -e spawn-monsters=$monster -e spawn-animals=$animal  -p 25565:$port itzg/minecraft-server --restart always

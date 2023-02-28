17b682b953e147c205a7e16737c2badcf50829da
Alan Newingham <48245017+01000001-01001110@users.noreply.github.com>
2023-02-28 16:21:28 -0500

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-# Update and upgrade packages
-echo "Update and upgrade packages"
-sudo apt-get update && sudo apt-get upgrade -y
-echo "Completed Updates and upgrades of packages"
-
-# Download Docker Compose
-echo "Download Docker Compose"
-sudo curl -L 'https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Add Docker GPG key
-echo "Add Docker GPG key"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-echo "Completed adding the Docker GPG key"
-
-# Add Docker repository
-echo "Add Docker repository"
-sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'
-echo "Completed adding the Docker repository"
-
-# Update and upgrade packages
-echo "Update and upgrade packages"
-sudo apt-get update && sudo apt-get upgrade -y
-echo "Completed Updates and upgrades of packages"
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce
+sudo systemctl status docker

d16a85084794edea545c47cccd4b798ba760d4a5
Alan Newingham <48245017+01000001-01001110@users.noreply.github.com>
2023-02-28 16:24:15 -0500

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt update
+sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce
+sudo apt update -y
+sudo apt install docker-ce  -y
 sudo systemctl status docker
